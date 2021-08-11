5375ababfce72a43bce3644019980370e22361f5
simeo wafula <xymonwafooler@gmail.com>
2021-08-11 09:06:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,45 +14,73 @@ namespace ETL_2
 {
     class Program
     {
+        public static string connectStat;
         public static void Main(string[] args)
         {
+   
+
+            /////////////////decrypting//////////////////////
+            var customerPath = @"C:\Users\USER\Documents\GitHub\SUNAMI SOLAR TRAINING PROJECT\SUNAMI-SOLAR-TRAINING-PROJECT\Project 2_3\customers-encrypted.csv";
+            var devicesPath = @"C:\Users\USER\Documents\GitHub\SUNAMI SOLAR TRAINING PROJECT\SUNAMI-SOLAR-TRAINING-PROJECT\Project 2_3\devices-encrypted.csv";
+
+            Console.WriteLine("Hi! Please choose file path to decrypt");// propmpts user to pick file to decrypt
+            var path = Console.ReadLine();
+            if (path == "c")
+            { path = customerPath; }
+            else if (path == "d")
+            { path = devicesPath; }
 
 
+            var encodedData = getPathdata(path);
+            var decoded = Decoded(encodedData);
+            Console.WriteLine("decoded data\n" + decoded);
+            ////////////////////////done decrypting//////////////////////////////
+            
+            //////text parsing/////////////////////////
+          
+            string[] parsed = decoded.Split(',');
 
-
-
-            /////connct to db///////
-
-
-            /////end of connection   
-
-
+            foreach (var word in parsed)
+            {
+                System.Console.WriteLine($"<{word}>");
+            }
+            ConnectDb();
+            Console.WriteLine(connectStat);
 
             //start of db creation
-            ///////////////conection code
-            string connetionString = null;
-            SqlConnection cnn;
-             connetionString = "server=localhost;user=root;database=world;port=3306;password=beee002j2011";
-            // connetionString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23";
-            //connetionString = @"Data Source=127.0.0.1;port=3306;Initial Catalog=users;User ID=root;Password=beee002j2011";
-            cnn = new SqlConnection(connetionString);
-            try
-            {
-                cnn.Open();
-                Console.WriteLine("Connection Open ! ");
-                cnn.Close();
-            }
-            catch (Exception ex)
-            {
 
-                Console.WriteLine("Can not open connection !");
-            }
 
 
             // end of db crearion
 
 
         }
+
+        public static String ConnectDb()//fetches encoded file content
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+       
+
+            myConnectionString = "server=127.0.0.1;uid=root;" + "pwd=beee002j2011;database=world;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection(myConnectionString);
+                conn.Open();
+                connectStat = "Succesfull connection!";
+                return connectStat;
+               // conn.Close();
+            }
+
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                connectStat="Can not open connection !";
+                return connectStat;
+            }
+        }
+
         public static String getPathdata(String path)//fetches encoded file content
         {
             Console.WriteLine("File Path\n" + path);
